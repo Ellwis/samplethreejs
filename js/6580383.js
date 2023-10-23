832eabc02e9191a83fe2802f25e265ff1d22062e
Mohammad Amiri <113034829+Ellwis@users.noreply.github.com>
2023-10-23 17:11:32 +0330

    file uploaded

---
+++ js/6580383.js
@@ -0,0 +1,71 @@
+!(function (t, e, a) {
+  if (!document.getElementById(t)) {
+    var s = document.createElement("script");
+    for (var r in ((s.src = "https://js.hsleadflows.net/leadflows.js"),
+    (s.type = "text/javascript"),
+    (s.id = t),
+    a))
+      a.hasOwnProperty(r) && s.setAttribute(r, a[r]);
+    var n = document.getElementsByTagName("script")[0];
+    n.parentNode.insertBefore(s, n);
+  }
+})("LeadFlows-6580383", 0, {
+  crossorigin: "anonymous",
+  "data-leadin-portal-id": 6580383,
+  "data-leadin-env": "prod",
+  "data-loader": "hs-scriptloader",
+  "data-hsjs-portal": 6580383,
+  "data-hsjs-env": "prod",
+  "data-hsjs-hublet": "na1",
+}),
+  (function (t, e) {
+    if (!document.getElementById(t)) {
+      var a = document.createElement("script");
+      (a.src =
+        "https://js.hs-analytics.net/analytics/1698063900000/6580383.js"),
+        (a.type = "text/javascript"),
+        (a.id = t);
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(a, s);
+    }
+  })("hs-analytics");
+var _hsp = (window._hsp = window._hsp || []);
+_hsp.push(["addEnabledFeatureGates", []]),
+  (function (t, e, a) {
+    if (!document.getElementById(t)) {
+      var s = document.createElement("script");
+      for (var r in ((s.src = "https://js.hs-banner.com/6580383.js"),
+      (s.type = "text/javascript"),
+      (s.id = t),
+      a))
+        a.hasOwnProperty(r) && s.setAttribute(r, a[r]);
+      var n = document.getElementsByTagName("script")[0];
+      n.parentNode.insertBefore(s, n);
+    }
+  })("cookieBanner-6580383", 0, {
+    "data-cookieconsent": "ignore",
+    "data-hs-ignore": !0,
+    "data-loader": "hs-scriptloader",
+    "data-hsjs-portal": 6580383,
+    "data-hsjs-env": "prod",
+    "data-hsjs-hublet": "na1",
+  }),
+  (function (t, e, a) {
+    if (!document.getElementById(t)) {
+      var s = document.createElement("script");
+      for (var r in ((s.src = "https://js.hsadspixel.net/fb.js"),
+      (s.type = "text/javascript"),
+      (s.id = t),
+      a))
+        a.hasOwnProperty(r) && s.setAttribute(r, a[r]);
+      var n = document.getElementsByTagName("script")[0];
+      n.parentNode.insertBefore(s, n);
+    }
+  })("hs-ads-pixel-6580383", 0, {
+    "data-ads-portal-id": 6580383,
+    "data-ads-env": "prod",
+    "data-loader": "hs-scriptloader",
+    "data-hsjs-portal": 6580383,
+    "data-hsjs-env": "prod",
+    "data-hsjs-hublet": "na1",
+  });
